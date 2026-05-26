8af28c705bfff06a7081f883053cc94dba865015
ODiogorocha <diogorocharsrs@gmail.com>
2026-05-25 22:16:09 -0300

    feat: create logic conection in database

--- javalin/src/main/java/demo/service/DatabaseLoader.java
+++ javalin/src/main/java/demo/service/DatabaseLoader.java
@@ -1,4 +1,59 @@
 package demo.service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.util.stream.Stream;
+
+
 public class DatabaseLoader {
+
+    private final Connection connection;
+    private final String pastaScripts = "database/";
+
+    public DatabaseLoader(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void inicializarBanco() {
+        executarScript("criacao_banco.sql");
+        executarScript("dados_iniciais.sql");
+
+        System.out.println("BANCO INICIADO");
+    }
+
+    public void executarScript(String nomeArquivo) {
+        Path caminho = Paths.get(pastaScripts + nomeArquivo);
+
+        if (!Files.exists(caminho)){
+            throw new RuntimeException("Arquivo SQL nao encontrado: " +  nomeArquivo);
+        }
+
+        try {
+            String conteudo = Files.readString(caminho);
+            String[] comandos = conteudo.split(";");
+
+            try (Statement stmt = connection.createStatement()) {
+                for (String comando : comandos) {
+                    comando = comando.trim();
+
+                    if (!comando.isEmpty()) {
+                        stmt.execute(comando);
+                    }
+                }
+            }
+            System.out.println("BANCO EXECUTADO");
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao executar script SQL: " + nomeArquivo, e);
+        }
+    }
+
+    public void resetarBanco() {
+        executarScript("limpar_banco.sql");
+        executarScript("banco resetado com suceso");
+    }
+
 }
